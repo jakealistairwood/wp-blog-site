042fbf7db7cf88fb861dcbca40667588bd826b43
Jake Wood <jakealistairwood@gmail.com>
2021-07-04 21:05:35 +0100

    saving changes

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+        <footer class="footer">
+            <div class="container">
+                <h4 class="footer-logo">Haven</h4>
+            </div>    
+        </footer>
         <?php wp_footer(); ?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -9,7 +9,9 @@
     <body <?php body_class(); ?>>
         <header class="header container">
             <div class="header__logo">
-                <h4 class="logo">Haven</h4>
+                <h4 class="logo">
+                    <a href="<?php echo site_url('/') ?>">Haven</a>
+                </h4>
                 <p class="logo-subheader">A wellbeing space dedicated to Introverts.</p>
             </div>
             <div class="header__links">
@@ -20,12 +22,12 @@
         </header>
         <nav class="navbar">
             <ul class="navbar__nav-links">
-                <li>Self-Development</li>
-                <li>Mental Health</li>
-                <li>Health & Wellbeing</li>
-                <li>Personality</li>
-                <li>Psychology</li>
-                <li>Creativity</li>
-                <li>Nutrition</li>
+                <li><a href="#">Self-Development</a></li>
+                <li><a href="<?php echo site_url('/category/mental-health') ?>">Mental Health</a></li>
+                <li><a href="#">Health & Wellbeing</a></li>
+                <li><a href="#">Personality</a></li>
+                <li><a href="#">Psychology</a></li>
+                <li><a href="#">Creativity</a></li>
+                <li><a href="#">Nutrition</a></li>
             <ul>
         </nav>
