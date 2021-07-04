3084b2682dd5ce31ba009761a474acd47bd41bda
Jake Wood <jakealistairwood@gmail.com>
2021-07-04 12:07:24 +0100

    wp-theme initial boilerplate in situ

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <title>Haven</title>
+        <meta charset="<?php bloginfo('charset') ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <nav class="navbar container">
+            <div class="navbar__nav-logo">
+                <h4 class="logo">Haven</h4>
+                <p class="logo-subheader">A wellbeing space dedicated to Introverts.</p>
+            </div>
+            <div class="navbar__nav-links">
+
+            </div>
+        </nav>
